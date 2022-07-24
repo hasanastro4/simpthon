8da0de9b5cce2a8e111b5de52286531502531b62
hasanastro4 <hasanuddin@physics.untan.ac.id>
2022-07-24 22:03:48 +0700

    update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,48 +34,49 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [ 'sphinx.ext.mathjax', 'sphinx.ext.napoleon', 'sphinx.ext.autodoc'
+extensions = [ 'sphinx.ext.mathjax', 'sphinx.ext.napoleon', 'sphinx.ext.autodoc',
+'sphinx.ext.linkcode'
 ]
 # from jobovy/galpy via
 # https://github.com/jobovy/galpy/blob/main/doc/source/conf.py
 # from disnake via:
 # https://twitter.com/readthedocs/status/1541830907082022913?s=20&t=eJ293FfjILT7sIxEyz834w
-#_simpthon_module_path = os.path.dirname(importlib.util.find_spec("simpthon").origin)
-#github_repo = "https://github.com/hasanastro4/simpthon"
+_simpthon_module_path = os.path.dirname(importlib.util.find_spec("simpthon").origin)
+github_repo = "https://github.com/hasanastro4/simpthon"
 # Current git reference. Uses branch/tag name if found, otherwise uses commit hash
-#def git(*args):
-#    return subprocess.check_output(["git", *args]).strip().decode()
-#git_ref= None
-#try:
-#    git_ref= git("name-rev", "--name-only", "--no-undefined", "HEAD")
-#    git_ref= re.sub(r"^(remotes/[^/]+|tags)/", "", git_ref)
-#except Exception:
-#    pass
-# (if no name found or relative ref, use commit hash instead)
-#if not git_ref or re.search(r"[\^~]", git_ref):
-#    try:
-#        git_ref = git("rev-parse", "HEAD")
-#    except Exception:
-#        git_ref = "main"
-#def linkcode_resolve(domain, info):
-#    if domain != "py":
-#        return None
-#    try:
-#        obj= sys.modules[info["module"]]
-#        for part in info["fullname"].split("."):
-#            obj= getattr(obj, part)
-#        obj= inspect.unwrap(obj)
+def git(*args):
+    return subprocess.check_output(["git", *args]).strip().decode()
+git_ref= None
+try:
+    git_ref= git("name-rev", "--name-only", "--no-undefined", "HEAD")
+    git_ref= re.sub(r"^(remotes/[^/]+|tags)/", "", git_ref)
+except Exception:
+    pass
+#if no name found or relative ref, use commit hash instead)
+if not git_ref or re.search(r"[\^~]", git_ref):
+    try:
+        git_ref = git("rev-parse", "HEAD")
+    except Exception:
+        git_ref = "main"
+def linkcode_resolve(domain, info):
+    if domain != "py":
+        return None
+    try:
+        obj= sys.modules[info["module"]]
+        for part in info["fullname"].split("."):
+            obj= getattr(obj, part)
+        obj= inspect.unwrap(obj)
 
-#        if isinstance(obj, property):
-#            obj= inspect.unwrap(obj.fget)
+        if isinstance(obj, property):
+            obj= inspect.unwrap(obj.fget)
 
-#        path= os.path.relpath(inspect.getsourcefile(obj),start=_simpthon_module_path)
-#        src, lineno = inspect.getsourcelines(obj)
-#    except Exception:
-#        return None
+        path= os.path.relpath(inspect.getsourcefile(obj),start=_simpthon_module_path)
+        src, lineno = inspect.getsourcelines(obj)
+    except Exception:
+        return None
 
-#    path = f"{path}#L{lineno}-L{lineno + len(src) - 1}"
-#    return f"{github_repo}/blob/{git_ref}/simpthon/{path}"
+    path = f"{path}#L{lineno}-L{lineno + len(src) - 1}"
+    return f"{github_repo}/blob/{git_ref}/simpthon/{path}"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
